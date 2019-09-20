acf5e307f96f593e7351e0660886fcbe5087669d
Brad <Brad@BRADSPC>
2019-09-20 12:08:46 -0600

    las tpush

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -25,9 +25,10 @@ const ColorList = ({ colors, updateColors }) => {
       .then(res => {
         console.log("edit result: ", res)
       })
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+      .then(() => {
+        axiosWithAuth().get('http://localhost:5000/api/colors')
+        .then((res) => {updateColors(res.data)})
+      })
   };
 
   const deleteColor = color => {
