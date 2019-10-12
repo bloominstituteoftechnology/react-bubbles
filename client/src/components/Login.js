88cc50bdbb2acb40b1055dc827dd23a14ccbbd18
AidenBow <AidenBow@users.noreply.github.com>
2019-10-11 18:21:12 -0700

    created PrivateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,31 +1,43 @@
 import React, {useState} from "react";
 
+const initialState = {
+  username: "",
+  password: ""
+}
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  const [login, setLogin] = useState("")
+  const [login, setLogin] = useState(initialState)
 
   const handleChanges = e => {
     setLogin({ ...login, [e.target.name] : e.target.value})
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(login)
+    setLogin(initialState)
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
           type="text"
           name="username"
           placeholder="username"
           onChange={handleChanges}
+          value={login.username}
         />
         <input 
           type="password"
           name="password"
           placeholder="password"
           onChange={handleChanges}
+          value={login.password}
         />
-        <button>Login</button>
+        <button type="submit">Login</button>
       </form>
     </>
   );
