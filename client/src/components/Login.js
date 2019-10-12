f30e09ec84212a323b45c3f40ca970bff5167dd8
Dominick Bruno <dbruno@unseen.is>
2019-10-11 22:07:58 -0400

    Axios with Auth / Private Route / and work on login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,18 @@
 import React from "react";
+import axios from "axios";
 
-const Login = () => {
+
+const Login = (props) => {
+  const userCredentials = { username: 'Lambda School', password: 'i<3Lambd4' };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  axios.post('/api/login', userCredentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      props.history.push('/bubblepage');
+    }
+    )
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
