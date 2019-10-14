89571cdf6406a5e62745ed26e4da30492896067d
Dave Kane <davelkane@gmail.com>
2019-10-13 20:28:05 -0500

    Login component complete

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,49 @@
 import React from "react";
+import { Form, Segment, Header } from 'semantic-ui-react'
+import axios from 'axios'
+import { useFormInput } from '../hooks';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+
+const Login = props => {
+  
+  const [values, changeHandler] = useFormInput({ username: '', password: '' })
+
+  const logInHandler = () => {
+    axios
+      .post(`http://localhost:5000/api/login`, values)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubblepage')
+      })
+      .catch(err => console.log('LogIn: POST:', err))
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <Segment>
+      <Segment>
+        <Header>Log In</Header>
+        <Form
+          onSubmit={logInHandler}>
+          <Form.Input 
+            fluid
+            label='Username'
+            name='username'
+            placeholder='Username'
+            value={values.username}
+            onChange={changeHandler}
+          />
+          <Form.Input 
+            fluid
+            label='Password'
+            name='password'
+            placeholder='Password'
+            value={values.password}
+            onChange={changeHandler}
+          />
+          <Form.Button>Log In</Form.Button>
+        </Form>         
+      </Segment>
+    </Segment>
   );
 };
 
