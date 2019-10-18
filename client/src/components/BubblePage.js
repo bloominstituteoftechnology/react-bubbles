10522f816bed9be47b0e1d61dd36da5c45eb888d
Afia-Lydia <you@example.com>
2019-10-18 10:13:14 -0400

    got bubbles to renduer using axios call

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+	const [colorList, setColorList] = useState([]);
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+	const fetchColors = () => {
+		axiosWithAuth()
+			.get("api/colors")
+			.then(res => {
+				console.log(res);
+				setColorList(res.data);
+			});
+	};
+	// fetch your colors data from the server when the component mounts
+	// set that data to the colorList state property
+
+	React.useEffect(() => {
+		fetchColors();
+	}, []);
+
+	return (
+		<>
+			<ColorList colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</>
+	);
 };
 
 export default BubblePage;
