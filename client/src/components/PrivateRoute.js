ff3abad89ede47f5c70adea96847c64d9782e56b
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-18 09:30:47 -0600

    private route set up in app.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -11,7 +11,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         if (token) {
           return <Component {...props} />;
         }
-        return <Redirect to="/Login" />;
+        return <Redirect to="/" />;
       }}
     />
   );
