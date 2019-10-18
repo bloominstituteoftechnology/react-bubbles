1751ef1266a9cf0887accbd5e8a26061125f0ad1
CScori <amilanotte1366@gmail.com>
2019-10-18 10:37:04 -0500

    adds login details and clean up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/AxiosWithAuth";
 
 const Login = (props) => {
   const [credentials, setCredentials] = useState({
     username: '',
     password: ''
   })
+
+  const login = e => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post('/api/login', credentials)
+    .then(res => {
+      console.log('Login Credentials', res.data)
+      localStorage.setItem('token', res.data.payload)
+      props.history.push('/bubbles')
+    })
+    .catch(err => console.log(err))
+  }
+
+  const handleChange = e => {
+    setCredentials({...credentials, [e.target.name]:e.target.value})
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
