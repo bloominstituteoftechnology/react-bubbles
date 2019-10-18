c737d62bfbd87f13c115512d39642063181e204c
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-10-18 11:23:05 -0400

    login form created. token sent to local storange. Authaxios setup complete

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+import { prependOnceListener } from "cluster";
 
-const Login = () => {
+
+const Login = (props) => {
+  const [form, setForm] = useState({username:"",password:""})
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const  handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post("/api/login", form)
+    .then(res =>{
+        console.log("post res",res)
+        localStorage.setItem("token",res.data.payload)
+        props.history.push()
+    })
+
+  }
+
+  const handleChanges = e => {
+    setForm({...form, [e.target.name] : e.target.value})
+  };
   return (
     <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" value={form.username} placeholder="username..." onChange={handleChanges}></input>
+        <input type="password" name="password" value={form.password} placeholder="password..." onChange={handleChanges}></input>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+      
+      
     </>
   );
 };
