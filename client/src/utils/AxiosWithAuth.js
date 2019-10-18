f53fb08e87af4484c2b0e01c9be3d4d7c0b5b631
CScori <amilanotte1366@gmail.com>
2019-10-18 09:45:01 -0500

    adding in auth token

--- client/src/utils/AxiosWithAuth.js
+++ client/src/utils/AxiosWithAuth.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import axios from 'axios'
 
-const AxiosWithAuth = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token')
+
+    return axios.create({
+        baseURL: 'http://localhost:5000',
+        headers: {
+            Authorization: token
+        }
+    })
 }
-
-export default AxiosWithAuth
