8c5527b1222f0d8e7a41cda959757f0f12db2137
Aaron Clark <aaronpclark92@gmail.com>
2019-10-18 10:22:37 -0500

    added getData to make axiosWtihAuth call to api/colors, passed function into useEffect to change state

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -2,11 +2,27 @@ import React, { useState, useEffect } from "react";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {
+    axiosWithAuth()
+      .get('/colors')
+      .then(res => {
+        console.log(`aWA res.data`, res.data)
+        setColorList(res.data)
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <>
