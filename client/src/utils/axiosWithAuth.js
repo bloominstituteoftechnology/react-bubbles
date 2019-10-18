493b08de8c66dd90d914985909000c89b6bebc8c
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-18 10:14:51 -0600

    refactored export

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const axiosWithAuth = () => {
+export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
+
   return axios.create({
     headers: {
       Authorization: token,
     },
   });
 };
-
-export default axiosWithAuth;
