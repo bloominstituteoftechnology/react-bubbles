a193338a7e83b05e8b744c3797d19ae08007a1c8
Greg <gsc229@gmail.com>
2019-10-18 11:59:32 -0400

    colors rendering to screen

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,30 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    getColors();
+  }, []);
+
+  const getColors = () => {
+    axiosWithAuth()
+      .get(`/api/colors`)
+      .then(res => {
+        console.log('BubblePage.js .get res', res);
+        setColorList(res.data);
+      })
+      .catch(err => {
+        alert(err.response);
+      });
+  };
 
   return (
     <>
