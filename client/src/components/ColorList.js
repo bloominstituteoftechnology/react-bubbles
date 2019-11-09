74bf9a9a930f4bf6d08990b8343f92dd30d80286
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-11-09 14:02:38 -0500

    stretch

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import api from "../utils/api";
 
 const initialColor = {
@@ -8,9 +7,14 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  console.log(colors)
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [createColor, setCreateColor] = useState({
+    code: {hex: ''},
+    color: '',
+    id: Date.now()
+  })
 
   const editColor = color => {
     setEditing(true);
@@ -27,7 +31,8 @@ const ColorList = ({ colors, updateColors }) => {
       alert('Color Updated!')
       api().get('/api/colors')
       .then(res => updateColors(res.data))
-      .catch(err => console.log(err))     
+      .catch(err => console.log(err))
+      setEditing(false);     
     })
     .catch(err => {
       console.log(err)
@@ -42,11 +47,30 @@ const ColorList = ({ colors, updateColors }) => {
       api().get('/api/colors')
       .then(res => updateColors(res.data))
       .catch(err => console.log(err))
+      setEditing(false);
     })
     .catch(err => {
       console.log(err)
     })
   };
+
+  const createColorSubmit = e =>{
+    e.preventDefault();
+    api().post('/api/colors', createColor)
+    .then(()=>{
+      alert('color created')
+      api().get('/api/colors')
+      .then(res => updateColors(res.data))
+      .catch(err => console.log(err))
+    })
+  }
+  const createOnChange = e =>{
+    e.preventDefault();
+    setCreateColor({
+      ...createColor,
+      [e.target.name]: e.target.value
+    })
+  }
 
   return (
     <div className="colors-wrap">
@@ -101,8 +125,29 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
-      
+      <div className="spacer">
+        <h1>Add A Color</h1>
+      <form className="addColor" onSubmit={createColorSubmit}>
+      <input
+        type="text"
+        name="color"
+        value={createColor.color}
+        onChange={createOnChange}
+        placeholder="Color Name"
+        style={{width:'95%', margin: '0 auto'}}
+        />
+        <input
+        type="text"
+        name="code"
+        value={createColor.code.hex}
+        onChange={e => setCreateColor({...colorToEdit, code: {hex: e.target.value}})}
+        placeholder="hex code"
+        style={{width:'95%', margin: '0 auto'}}
+        />
+        
+        <button type="submit">Create!</button>
+      </form>
+      </div>
     </div>
   );
 };
