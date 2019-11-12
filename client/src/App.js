90a8006dd0fcd2b104ec411d7290d028d1968e76
Jordan Lanier <jordan.6195@yahoo.com>
2019-11-11 20:51:15 -0500

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import BubblePage from './components/BubblePage';
+import Login from './components/Login';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import './styles.scss';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<nav className="nav">
+					<h1>Bubbles</h1>
+					<ul>
+						<li>
+							<Link to="/">Login</Link>
+						</li>
+						<li>
+							<Link to="/bubblepage">Bubbles</Link>
+						</li>
+					</ul>
+				</nav>
+				<Switch>
+					<Route exact path="/" component={Login} />
+					<Route path="/bubblepage" component={BubblePage} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,68 @@
-import React from "react";
+import React from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { Redirect } from 'react-router-dom';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+	state = {
+		credentials: {
+			username: '',
+			password: ''
+		}
+	};
+
+	handleChange = e => {
+		this.setState({
+			credentials: {
+				...this.state.credentials,
+				[e.target.name]: e.target.value
+			}
+		});
+	};
+
+	login = e => {
+		e.preventDefault();
+		axiosWithAuth()
+			.post('/api/login', this.state.credentials)
+			.then(res => {
+				localStorage.setItem('token', res.data.payload);
+				this.props.history.push('/bubblepage');
+			})
+			.catch(err => console.log('Access denied', err.response));
+	};
+
+	render() {
+		if (localStorage.getItem('token')) return <Redirect to="bubblepage" />;
+		return (
+			<div>
+				<h1>Welcome to the Bubble App!</h1>
+				<div className="form">
+					<form>
+						<input
+							type="text"
+							name="username"
+							value={this.state.credentials.username}
+							placeholder="username"
+							autoComplete="username"
+							onChange={this.handleChange}
+						/>
+
+						<input
+							type="password"
+							name="password"
+							value={this.state.credentials.password}
+							placeholder="password"
+							autoComplete="current-password"
+							onChange={this.handleChange}
+						/>
+
+						<button type="submit" onClick={this.login}>
+							Login
+						</button>
+					</form>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Login;
