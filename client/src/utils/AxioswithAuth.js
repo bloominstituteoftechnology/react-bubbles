18b98a67b88a4dbf5dc8655a90841ffb43da4b9f
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-15 11:22:13 -0500

    added axioswithAuth file

---
+++ client/src/utils/AxioswithAuth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+  
+//   console.log(key)
+  return axios.create({
+    baseURL: 'https://localhost:5000/',
+    headers: {
+      Authorization: 
+      "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
+    }
+  });
+}
