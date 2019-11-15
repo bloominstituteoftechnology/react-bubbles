ca669635ca6efff21a6aa0447b65de3cb296ae82
Chrismis79 <chrismis79@gmail.com>
2019-11-15 11:21:07 -0600

    Bubblepg done and colors/bubbles are rendering!

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const [newGet, setNewGet] = useState();
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    setNewGet(false);
+    axiosWithAuth()
+      .get("/colors")
+      .then(res => {
+        console.log("GET", res.data);
+        setColorList(res.data);
+      })
+      .catch(err => console.log(err));
+  }, [newGet]);
+
+
 
   return (
     <>
