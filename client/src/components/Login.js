6cf3cb30bb2b08cb6b06710401350c587ff39a1e
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-11-15 14:09:10 -0500

    stretch

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React,{useState} from "react"
+import axiosWithAuth from "../axiosWithAuth"
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const Login = props => {
+    const [error,setError] = useState()
+const [data,setData] = useState({
+    username:"",
+    password:""
+})
+const [isLoading,setLoading] = useState("")
+const handleChange = e => {
+    setData({
+        ...data,
+        [e.target.name]:e.target.value
 
-export default Login;
+    })
+    console.log(data)
+}
+// { username: 'Lambda School', password: 'i<3Lambd4' }
+const handleSubmit = e => {
+    e.preventDefault()
+    console.log(data)
+    axiosWithAuth()
+    .post("/api/login",
+        data
+    )
+.then(res => {
+    localStorage.setItem("token",res.data.payload)
+    props.history.push("/BubblesPage")
+    console.log(res.data.payload)
+})
+.catch(err => setError(err))
+}
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {error && <div className="error">error</div>}
+        <input placeholder="username" value={data.username} name="username" type="username" onChange={handleChange}/>
+        <input placeholder="Password" value={data.password} name="password" type="password" onChange={handleChange}/>
+        <button>Submit</button>
+        </form>
+    )
+}
+export default Login
