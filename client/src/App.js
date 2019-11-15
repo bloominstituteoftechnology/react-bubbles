c434562e301836f888cd5e4432c6a7f10d9a6546
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-15 11:27:31 -0500

    added private route setup to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 
+import PrivateRoute from './utils/PrivateRoute';
+import BubblePage from './components/BubblePage';
+
 function App() {
   return (
     <Router>
@@ -13,6 +16,10 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+    <PrivateRoute path='/protected'>
+        <BubblePage/>
+    </PrivateRoute>
+
       </div>
     </Router>
   );
