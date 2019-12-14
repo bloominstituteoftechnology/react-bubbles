bad71ce3dad9e1f6bc8d6c59261b4a920320e3b8
Veto Ramirez <vetorobert3@gmail.com>
2019-12-14 11:33:18 -0800

    create a axios auth component.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
+
+  return ( 
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
