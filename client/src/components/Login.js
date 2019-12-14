ebc0550c02c47f5ae9a4cda796771b48797edf90
Katrina_Dierking <kalynndier@gmail.com>
2019-12-14 13:29:05 -0500

    login page created

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,77 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from '../utils/axiosWithAuth'
+import styled from 'styled-components';
 
-const Login = () => {
+const Container = styled.div`
+  border: .5px solid grey;
+  margin-left:45%;
+  margin-top:10%;
+  padding:1%;
+  height:20%;
+  width: 10%;
+`
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState ({
+    username: '',
+    password: ''
+  });
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    axiosWithAuth()
+      .post('/login', credentials)
+      .then(result => {
+        console.log('kid:login:axios.post', result.data)
+        localStorage.setItem('token', result.data.payload);
+        props.history.push('/bubblepage');
+        console.log('kd:login:props', props)
+      })
+      .catch(error => 
+        console.log('kd:login:axios.catch:error', error));
+  };
+
+  const handleChange = event => {
+    setCredentials ({
+      ...credentials, 
+      [event.target.name] : event.target.value
+    })
+  };
+    
+  
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+    <Container>
+      <h2>Log In to Bubbles</h2>
+    <form onSubmit = {onSubmit}>
+        <input
+          type = 'text'
+          name = 'username'
+          placeholder = '* username'
+          value = {credentials.username}
+          onChange = {handleChange}
+        />
+        <br></br>
+        <br></br>
+
+      <input  
+        type = 'password'
+        name = 'passowrd'
+        placeholder = '* password'
+        value = {credentials.password}
+        onChange = {handleChange}
+        />
+        <br></br>
+        <br></br>
+
+        <button> Log In </button>
+      </form>
+    </Container>
+    
     </>
   );
 };
