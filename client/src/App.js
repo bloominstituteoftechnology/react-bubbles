e2779b69caf6a7ed4874507b754ed7d3c7bc7177
shgradyy <shantaygrady@gmail.com>
2019-12-16 18:33:25 -0500

    Builds login. Adds utils

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import PrivateRoute from "./utils/PrivateRoute.js";
-import BubblePage from "./components/BubblePage.js"
+
+
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -16,10 +16,11 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-        <PrivateRoute path="/bubbles" component={BubblePage} />
+      
       </div>
     </Router>
   );
 }
+
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,34 @@
 import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth.js";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+  const credentials = { username: "Lambda School", password: "i<3Lambd4" };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  };
+
   return (
-    <>
+    <div className="login">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <p>Please enter your account information below!</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" placeholder="Lambda School" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" placeholder="i<3Lambd4" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
-export default Login;
