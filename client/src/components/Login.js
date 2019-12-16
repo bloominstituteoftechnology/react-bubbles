6c39dd062552533c9c43a5a12935de9c402e40c2
Veto Ramirez <vetorobert3@gmail.com>
2019-12-15 19:01:31 -0800

    Need to fix code in the login page.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios';
 import { axiosWithAuth } from '../authWithAxios';
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({});
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth().post('login/endpoint', credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.token);
+        this.props.history.push('/');
+      })
+  }
+
+  const handleChange = e => {
+    setCredentials: {
+      ...credentials,
+      [e.target.name]: e.target.value,
+    }
+  } 
 
   return ( 
     <>
