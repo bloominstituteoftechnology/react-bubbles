fd33e0606098167bc331ea1247713f29c8353b1d
shgradyy <shantaygrady@gmail.com>
2019-12-16 20:51:04 -0500

    Updates BubblePage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,7 @@ const Login = props => {
   const handleSubmit = e => {
     e.preventDefault();
     axiosWithAuth()
-      .post("http://localhost:5000/api/login", credentials)
+    .post("login", credentials)
       .then(res => {
         localStorage.setItem("token", res.data.payload);
         props.history.push("/bubbles");
