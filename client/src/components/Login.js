6746778e503552a2e8e88a0c2cd70e6c72713f89
shgradyy <shantaygrady@gmail.com>
2019-12-18 19:21:43 -0500

    resets state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,10 +3,24 @@ import axiosWithAuth from "../utils/axiosWithAuth.js";
 
 const Login = props => {
   
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
+    setCredentials({ username: '', password: '' });
+
     axiosWithAuth()
-    .post("login")
+    .post("login", credentials)
       .then(res => {
         localStorage.setItem("token", res.data.payload);
         props.history.push("/bubbles");
@@ -22,9 +36,9 @@ const Login = props => {
       <p>Please enter your account information below!</p>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
-        <input type="text" name="username" placeholder="Lambda School" />
+        <input type="text" name="username" placeholder="Lambda School" value={credentials.password} onChange={handleChange} />
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password" placeholder="password" />
+        <input type="password" name="password" placeholder="password" value={credentials.password} onChange={handleChange}  />
         <button type="submit">Login</button>
       </form>
     </div>
