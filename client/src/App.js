8e3c9071b58539d0392f332bfcebbb8b3b8f4d37
Lambda_School_Loaner_222 <lambda_school_loaner_222@Lambda-School-Loaner-222s-MacBook-Pro.local>
2019-12-20 10:26:31 -0800

    mvp and stretch

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute.js";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/dash" component={BubblePage} />
       </div>
     </Router>
   );
 }
 
 export default App;
+//pword i<3Lambd4
