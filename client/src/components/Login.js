9d1e6f8f1f3781948d07cd9058db022e31d3c66d
Aasa-Christian <aasachristian@yahoo.com>
2019-12-20 11:27:27 -0500

    creted axios with auth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import AxiosWithAuth from "../utilz/AxiosWithAuth";
 
-const Login = () => {
+const Login = (e) => {
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const [isFetching, setIsFetching] = useState(false)
+
+
+  const login = e => {
+    e.preventDefault();
+    setIsFetching(true);
+    AxiosWithAuth()
+    .post('/login', credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload);
+      PaymentResponse.history.push('/')
+    })
+    .catch(err => console.log(err))
+  }
+  
+
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
