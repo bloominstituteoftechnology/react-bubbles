d9a339a2e0994ba2c69376607aab29f7d2276c6e
DeniseFafette <denisefafette@gmail.com>
2019-12-20 10:11:47 -0800

    added axios.get to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/items')
+      .then(res => setItems(res.data))
+      .catch(error => console.log(error));
+  }, []);
+  
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+          <Switch>
+            <PrivateRoute path="/protected" component={BubblePage} />
+            <Route path="/login" component={Login} />
+            <Route component={Login} />
+          </Switch>
       </div>
     </Router>
   );
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
 
 export function LoginForm() {
     const [form, setForm] = useState ({
