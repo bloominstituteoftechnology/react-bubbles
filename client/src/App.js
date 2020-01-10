7b3527274803383ba93396dd1764b2beb2cf705f
DeniseFafette <denisefafette@gmail.com>
2020-01-09 17:51:59 -0800

    completed sprint

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,18 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Login from "./components/Login";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { Login } from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
-function App() {
-
-  const [colors, setColors] = useState([]);
-  useEffect(() => {
-    axios
-      .get('http://localhost:3333/items')
-      .then(res => setColors(res.data))
-      .catch(error => console.log('get err', error));
-
-    axios
-      .post('http://localhost:3333/items')
-      .then(res => setColors(res.data))
-      .catch(error => console.log('post er', error));
-
-    axios
-      .delete('http://localhost:3333/items')
-      .then(res => setColors(res.data))
-      .catch(error => console.log('delete err', error));
-  }, []);
-
-  
+function App() { 
 
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-          <Switch>
             <PrivateRoute path="/protected" component={BubblePage} />
-            <Route path="/login" component={Login} />
-            <Route component={Login} />
-          </Switch>
+            
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,67 @@
-import React from "react";
-import LoginForm from "./LoginForm";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+// axiosWithAuth()
+// .post('/login', this.state.credentials)
+// .then(res => {
+//   localStorage.setItem('token', res.data.payload);
+//   this.props.history.push('/protected');
+// })
+// .catch(err => console.log(err));
+
+export const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>
-        Test
-      </p>
-    </>
-  );
-};
 
-export default Login;
+  // handle submit function and set state w/ useState ... confirm protects
+
+  const [form, setForm] = useState ({
+      username:'',
+      password:'',
+  })
+  console.log(form);
+
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      axiosWithAuth().post('/login', form)
+      .then(response => {
+          console.log(response)
+          localStorage.setItem('token', response.data.payload)
+          props.history.push('/protected')
+      })
+      .catch(err => console.table(err))
+  }
+
+  return(
+    <>
+    <h1>Welcome to the Bubble App!</h1>
+      <form onSubmit={handleSubmit}>
+      <input
+          name='username'
+          type='text'
+          placeholder='User name'
+          value={form.username}
+          onChange={(event) => {
+              setForm({
+                  ...form,
+                  [event.target.name]:event.target.value
+              })
+          }}
+          />
+      <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={form.password}
+          onChange={(event) => {
+              setForm({
+                  ...form,
+                  [event.target.name]:event.target.value
+              })
+          }}
+          />
+          <button type='submit'>Submit</button>
+      </form>
+      </>
+  )
+}
