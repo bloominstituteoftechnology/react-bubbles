12e440d84eb986cd4d2ab93636cf71c150c10496
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-01-24 20:34:02 -0500

    complete

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Pack } from "@potion/layout";
-import { Svg, Circle } from "@potion/element";
+import React, { useState, useEffect } from 'react';
+import { Pack } from '@potion/layout';
+import { Svg, Circle } from '@potion/element';
 
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
@@ -13,7 +13,7 @@ const Bubbles = ({ colors }) => {
   }, [colors]);
 
   return (
-    <div className="bubble-wrap">
+    <div className='bubble-wrap'>
       <p>bubbles</p>
       <Svg width={400} height={400}>
         <Pack
@@ -24,8 +24,7 @@ const Bubbles = ({ colors }) => {
           size={[400, 400]}
           includeRoot={false}
           nodeEnter={d => ({ ...d, r: 0 })}
-          animate
-        >
+          animate>
           {nodes =>
             nodes
               .map(({ x, y, r, key }, i) => {
