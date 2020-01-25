c846771b90c098df9fc1b6f6264cdfa2bfc3ad34
CLYPR Technologies <cbanks@clypr.co>
2020-01-25 13:08:00 -0500

    login form completed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,46 @@
 import React from "react";
+import { useForm } from 'react-hook-form';
+import axiosWithAuth from '../auth/axiosWithAuth';
+import PrivateRoute from "./PrivateRoute";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    reset();
+    axiosWithAuth()
+      .post('/login', data)
+      .then((res) => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push(`/private`);
+      })
+      .catch((err) => 
+        console.log('Error', err));
+    };
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit(onsubmit)}>
+        <input
+          type='text'
+          name='username'
+          placeholder='username'
+          ref={register}
+        />
+        <br></br>
+        <input
+          type='password'
+          name='password'
+          placeholder='password'
+          ref={register}
+        />
+        <br></br>
+        <input
+          type='submit'
+          value='Log In'
+        />
+      </form>
     </>
   );
 };
