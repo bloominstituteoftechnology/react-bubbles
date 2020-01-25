52b238097519321d8a8911ea8e87098d6a496883
CLYPR Technologies <cbanks@clypr.co>
2020-01-25 12:50:38 -0500

    completed PrivateRoute

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...restOfProps }) => {
+    return (
+        <Route
+            {...restOfProps}
+            render={() => {
+                if (localStorage.getItem(`token`)) {
+                    return <Component />;
+                } else {
+                    return <Redirect to='/' />;
+                }
+            }}
+        />
+    );
+};
+
+export default PrivateRoute;
