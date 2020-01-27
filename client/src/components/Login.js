2490b2a0aaa331062506646e050ca9cae0e2e8c0
Michael Lowe <mrlowememphis@yahoo.com>
2020-01-26 20:17:28 -0600

    MVP met styled components added to Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -50,3 +50,41 @@ const Login = props => {
 };
 
 export default Login;
+
+// Adding styling
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background:  	#2dc8aa;
+`;
+
+const Button = styled.button`
+  background: #ee9701;
+  border-radius: 20px;
+  padding: 10px;
+  margin: 5px;
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin: 10px;
+  border-radius: 20px;
+  border: 1px black;
+`;
+
+const Form = styled.form`
+  background: #89DCF5;
+  border-radius: 15px;
+  border: 7px ridge rgba(28,110,164,0.77);
+  padding: 40px;
+  margin: 0;
+  box-shadow: 10px 10px 8px #888888;
+`;
+
+
+
