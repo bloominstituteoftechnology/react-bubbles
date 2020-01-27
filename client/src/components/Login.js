cedef27d04787a49a13c223cab2cc0840d18c549
Michael Lowe <mrlowememphis@yahoo.com>
2020-01-26 19:08:07 -0600

    Depedencies, PrivateRoute and Login added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-const Login = () => {
+const Login = props => {
+  const [user, setUser] = useState({ username: '', password: '' });
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', user)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/bubbles');
+      })
+      .catch(error => console.log(error));
+  };
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+
   return (
-    <>
+    <Wrapper>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      
+      <Form onSubmit={login}>
+        <Input
+          type='text'
+          name='username'
+          value={user.username}
+          placeholder='Enter Username'
+          onChange={handleChange}
+        />
+        <Input
+          type='password'
+          name='password'
+          value={user.password}
+          placeholder='Enter Password'
+          onChange={handleChange}
+        />
+        <Button>Login</Button>
+      </Form>
+    </Wrapper>
   );
 };
 
