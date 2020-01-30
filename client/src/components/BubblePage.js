539b45017376852712dd2c1573509a1404e8636a
josegocampo <jose.ocampo@mail.udp.cl>
2020-01-30 17:28:12 -0300

    ...

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from "./axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,9 +8,20 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect( () => {
+    axiosWithAuth().get('http://localhost:5000/api/colors')
+    .then(response => {
+      console.log('this is the id', response)
+      setColorList(response.data);
+    }).catch(error => {
+      console.log(error)
+    });
+  }, []);
 
   return (
     <>
+
+    
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "./axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,13 +18,29 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    axiosWithAuth().put(`api/colors/${colorToEdit.id}`, colorToEdit)
+    .then( response => {
+      setEditing(true)
+    })
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
+
+  const deleteColor = (color) => {
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(() => {
+        axiosWithAuth()
+          .get('http://localhost:5000/api/colors')
+          .then(res => updateColors(res.data))
+          .catch(err => console.log(err))
+          setEditing(false);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      })
   };
 
   return (
