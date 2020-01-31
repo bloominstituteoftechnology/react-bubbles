17d72f318eb08f912da8758c4cf5926f3fd56c57
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-01-31 10:24:14 -0700

    axios with auth util added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 //Private Route
 import PrivateRoute from "./components/PrivateRoute";
@@ -15,6 +15,17 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <div>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/bubblepage">Bubbles</Link>
+          </li>
+        </ul>
+        </div>
+
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
