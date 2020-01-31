71c8379138992d517ac5f591ef96d851a8bf839e
nicbongo <nicbongo@protonmail.com>
2020-01-31 12:36:40 -0400

    login complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute"
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <nav>
+          <a>
+            <Link to="/login">Login</Link>
+          </a>
+          <a>
+            <Link to="/bubbles">Bubbles</Link>
+          </a>
+        </nav>
+
+        <Switch>
+          <PrivateRoute exact path ="/bubbles" component={BubblePage} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Login} />
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,10 +9,24 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  // useEffect(() => {
+  //   axios 
+  //   .get("http://localhost:5000/")
+  //     .then(res => {
+  //       console.log(`this is response from port 5000:`, res)
+  //     })
+  //     .catch(err => {
+  //       console.log(`this is error from port 5000:`, err)
+
+  //     })
+  // }, [])
+
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
+      {/* <ColorList colors={colorList} updateColors={setColorList} />
+      <Bubbles colors={colorList} /> */}
+
+      <h1>BUBBLES PAGE</h1>
     </>
   );
 };
