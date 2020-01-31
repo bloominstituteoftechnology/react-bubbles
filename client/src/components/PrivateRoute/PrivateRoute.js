27d69b2a2b1252217725c73497806479fe384c8b
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-31 11:19:09 -0500

    [Initial] Initial File & dependecy setup

---
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = () => {
+    return(
+        <div>
+            private
+        </div>
+    )
+}
+
+export default PrivateRoute;
