e15fd7f4e0a8bb4f9fb7a44d8d113829d64b1802
Timothy <timgill987@gmail.com>
2020-01-31 14:42:50 -0500

    mvp is not met

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import BubblePage from "./components/BubblePage"
 import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute'
 import "./styles.scss";
 
 function App() {
+  console.log()
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path="/protected" component={BubblePage}/>
+        
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
       </div>
+      
     </Router>
+    
   );
 }
 
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../Utils/Auth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
 
+  axiosWithAuth()
+    .get("/colors")
+    .then(res => {
+      console.log(res.data);
+      setColorList(...colorList, [res.data]);
+    })
+    .catch(err => console.log(err));
+
+    console.log(colorList, 'BubblePage')
   return (
-    <>
+    <div>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
-    </>
+    </div>
   );
 };
 
