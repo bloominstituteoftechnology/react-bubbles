1097aba46ecc9edffec6811630456f2343579756
Alexis <alexisdavalos.tech@gmail.com>
2020-01-31 14:00:04 -0500

    Tried to fix form validation, bug persists but MVP and Stretch Met

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,16 +11,35 @@ const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor)
   const [colorToAdd, setColorToAdd] = useState(initialColor);
+  const [error, setError] = useState(false);
   console.log('Colors in ColorList.js:',colors);
   console.log('colorToEdit in ColorList.js:', colorToEdit);
   console.log('colorToAdd in ColorList.js:', colorToAdd);
-
+  console.log('Error State:', error)
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
   const addColor = e => {
+    setError(false);
+    console.log('adding color...')
+    console.log(colorToAdd.color)
     e.preventDefault();
+    // if((colorToAdd.code.hex || colorToAdd.color) !== (colorToAdd.code.hex || colorToAdd.color))
+    // colors.forEach(item => {
+    //   console.log(item.color);
+    //   (((colorToAdd.code.hex === item.code.hex) || (colorToAdd.color === item.color)) ? setError(true) : setError(false))
+    // })
+    // // {(error) ? console.log('error') : console.log('no-error')}
+    // if(!error && (colorToAdd.code.hex !== '') && (colorToAdd.color !== '')){
+    //    axiosWithAuth()
+    //   .post('/api/colors', colorToAdd)
+    //   .then(res => (console.log('Added Color:', res), updateColors(res.data)))
+    //   .catch(err => console.log(err))
+    //   setError(false)
+    // }else{
+    //   setError(true);
+    // }
     axiosWithAuth()
     .post('/api/colors', colorToAdd)
     .then(res => (console.log('Added Color:', res), updateColors(res.data)))
@@ -31,18 +50,17 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    let newColors = [];
     axiosWithAuth()
     .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
-    .then(res => (console.log('Put Request Response:',res && colors), 
+    .then(res => (
+        console.log('Put Request Response:',res), 
         updateColors([...colors.filter(color => color.id !== res.data.id), res.data]),
-        console.log('Updated Colors', colors.sort()))
-
-        // newColors= [...colors.filter(color => color.id !== res.data.id), res.data],
-        // newColors.sort(),
-        // console.log('Updated Colors', newColors.reverse()))
-   
-
-    )
+        console.log('Updated Colors', colors.sort()),
+        newColors= [...colors.filter(color => color.id !== res.data.id), res.data],
+        console.log('newColors', newColors.sort((a,b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))),
+        updateColors(newColors.sort())
+    ))
     .catch(err => console.log(err))
     
   };
@@ -78,7 +96,37 @@ const ColorList = ({ colors, updateColors }) => {
           </li>
         ))}
       </ul>
-      {editing && (
+      
+      <div className="spacer" >
+        <form onSubmit={addColor}>
+            <legend>Add Color</legend>
+            <label>
+              color name:
+              <input
+                onChange={e =>
+                  setColorToAdd({ ...colorToAdd, color: e.target.value })
+                }
+                value={colorToAdd.color}
+              />
+            </label>
+            <label>
+              hex code:
+              <input
+                onChange={e =>
+                  setColorToAdd({
+                    ...colorToAdd,
+                    code: { hex: e.target.value }
+                  })
+                }
+                value={colorToAdd.code.hex}
+              />
+            </label>
+            <div className="button-row">
+              <button type="submit">Add New Color</button>
+            </div>
+            {(error) ? <p style={{color:'red'}}>Error: Check Values</p> : <p></p>}
+          </form>
+          {editing && (
         <form onSubmit={saveEdit}>
           <legend>Edit Color</legend>
           <label>
@@ -108,36 +156,8 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-
-      <div className="spacer" >
-        <form onSubmit={addColor}>
-            <legend>Add Color</legend>
-            <label>
-              color name:
-              <input
-                onChange={e =>
-                  setColorToAdd({ ...colorToAdd, color: e.target.value })
-                }
-                value={colorToAdd.color}
-              />
-            </label>
-            <label>
-              hex code:
-              <input
-                onChange={e =>
-                  setColorToAdd({
-                    ...colorToAdd,
-                    code: { hex: e.target.value }
-                  })
-                }
-                value={colorToAdd.code.hex}
-              />
-            </label>
-            <div className="button-row">
-              <button type="submit">Add New Color</button>
-            </div>
-          </form>
         </div>
+      
     </div>
   );
 };
