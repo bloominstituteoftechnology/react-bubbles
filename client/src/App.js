1177f3924758ba558ec6f762db255acdcbcfe53c
Timothy <timgill987@gmail.com>
2020-02-01 15:51:15 -0500

    found the bug! app redners and functions as intended per the MVP

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <PrivateRoute exact path ="/private" component={BubblePage} />
+        <PrivateRoute exact path ="/protected" component={BubblePage} />
         <Route exact path="/" component={Login} />
         <Route path="/login" component={Login} />
       
