2144059890c5903235e4ac2f5f193aabbe5161ab
Veto Ramirez <vetorobert3@gmail.com>
2020-02-22 11:36:00 -0800

    Working on bubble page.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,7 +9,17 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
-  
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/login')
+      .then(res => {
+        console.log(res)
+        setColorList(res.data)
+      .catch(err => {
+        console.log(err)
+      })
+      })
+  }, [])
 
   return (
     <>
