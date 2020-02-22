0b9d30510139edc98209a4db8c025e94d192b1f2
Shya Biswas <shyabiswas@gmail.com>
2020-02-21 23:15:24 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute"
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -8,11 +9,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path="/protected" component={PrivateRoute}/>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from "./AxiosWithAuth"
 
-const Login = () => {
+const Login = (props) => {
+  const [error, setError]= useState()
+  const [user, setUser] = useState({
+    username: "",
+    password: "",}
+  )
+  const handleChange = (event)=>{
+    setUser({...user, 
+      [event.target.name]: event.target.value,})
+  }
+  const handleSubmit = (event)=>{
+    event.preventDefault()
+    axiosWithAuth()
+    .post("/api/colors", user)
+    .then((result)=>{
+      console.log(result)
+      localStorage.setItem("token", result.data.token)
+      props.history.push("/protected")
+    })
+    .catch((err)=>{
+      setError(err.response.data)
+    })
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        {error && <div className="error">
+          {error}</div>}
+      <input
+        type="username"
+        name="username"
+        placeholder="username"
+        value= {user.username}
+        OnChange={handleChange}
+      />
+       <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value= {user.password}
+        OnChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
