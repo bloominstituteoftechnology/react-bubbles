01281c2002760d8518f3e1e790aa5fd62ea435aa
Vincent Adeniji <adenijitosin112@gmail.com>
2020-02-23 22:24:12 -0500

    MVP met

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import axios from "axios";
-
+import AxiosWithAuth from './axiosWithAuth'
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  // console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,12 +17,25 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    AxiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(() => {
+      AxiosWithAuth().get(`/colors`)
+      .then(res => updateColors(res.data))
+    })
+    .catch(err => console.log(err))
+    setEditing(false)
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
   const deleteColor = color => {
+    AxiosWithAuth().delete( `/colors/${color.id}`)
+    .then(() => {
+      AxiosWithAuth().get(`/colors`)
+      .then(res => updateColors(res.data))
+    })
+    .catch(err => console.log(err))
     // make a delete request to delete this color
   };
 
@@ -34,7 +46,7 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
+              <span className="delete" onClick={(e) => {
                     e.stopPropagation();
                     deleteColor(color)
                   }
