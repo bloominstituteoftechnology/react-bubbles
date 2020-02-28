cc933ec30ee4a14708e6fb281b5d66b6cb2dc325
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-02-28 12:58:34 -0600

    mVp one strectch

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState }from "react";
+import { useForm } from "react-hook-form";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+
+const [data, setData] = useState({});
+  const handleChange = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
+    axiosWithAuth()
+      .post("/login", data)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubblePage");
+      });
+    }
+    return (
+      <div>
+        <h1>Welcome to the Bubble App!</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={data.username}
+            onChange={handleChange}
+            ref={register({ required: true, maxLength: 80 })}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            ref={register({ required: true })}
+          />
+          <br />
+          <button onClick={handleSubmit}>log in</button>
+          
+        </form>
+      </div>
+    );
+  };
 
 export default Login;
