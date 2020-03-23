cf6879c1ab2daf4ea8714e0d494885bad19a8080
Nick Verdin <nv1028@aggienetwork.com>
2020-03-22 19:46:15 -0500

    stretch and add new color functionality sucessfully added

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -14,8 +14,11 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState({
+    color: "",
+  code: { hex: "" }
+  })
 
-  
   console.log('colors', colors)
 
   const editColor = color => {
@@ -28,62 +31,91 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-    e.preventDefault();
-   
+
+
     console.log('colorToEdit', colorToEdit)
     axiosWithAuth()
-        .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
 
-        .then(res => {
-            console.log('res inside put', res)
-            console.log('res.data', res.data);
-            // console.log('match.params.id inside put', match.params.id)
-            // props.getMovieList();
-            axiosWithAuth().get('http://localhost:5000/api/colors')
-                .then(res => {
-                   updateColors(res.data)
-                })
-                .catch(err => console.log(err))
-                console.log(res.data.payload);
-            history.push(`/`)
+      .then(res => {
+        console.log('res inside put', res)
+        console.log('res.data', res.data);
+        // console.log('match.params.id inside put', match.params.id)
+        // props.getMovieList();
+        axiosWithAuth().get('http://localhost:5000/api/colors')
+          .then(res => {
+            updateColors(res.data)
+          })
+          .catch(err => console.log(err))
+        console.log(res.data.payload);
+        history.push(`/`)
 
-        })
-        .catch(err => {
-            console.log('err inside catch', err);
-        })
+      })
+      .catch(err => {
+        console.log('err inside catch', err);
+      })
 
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
-    
+
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-    
-   
+
+
     console.log('colorToEdit', colorToEdit)
     axiosWithAuth()
-        .delete(`http://localhost:5000/api/colors/${color.id}`, color)
+      .delete(`http://localhost:5000/api/colors/${color.id}`, color)
 
-        .then(res => {
-            console.log('res inside put', res)
-            console.log('res.data', res.data);
-            // console.log('match.params.id inside put', match.params.id)
-            // props.getMovieList();
-            axiosWithAuth().get('http://localhost:5000/api/colors')
-                .then(res => {
-                   updateColors(res.data)
-                })
-                .catch(err => console.log(err))
-                console.log(res.data.payload);
-            history.push(`/`)
+      .then(res => {
+        console.log('res inside put', res)
+        console.log('res.data', res.data);
+        // console.log('match.params.id inside put', match.params.id)
+        // props.getMovieList();
+        axiosWithAuth().get('http://localhost:5000/api/colors')
+          .then(res => {
+            updateColors(res.data)
+          })
+          .catch(err => console.log(err))
+        console.log(res.data.payload);
+        history.push(`/`)
 
-        })
-        .catch(err => {
-            console.log('err inside catch', err);
-        })
+      })
+      .catch(err => {
+        console.log('err inside catch', err);
+      })
   };
+
+  const addColor = (e) => {
+    e.preventDefault();
+    console.log(newColor)
+
+    axiosWithAuth()
+      .post('http://localhost:5000/api/colors', newColor)
+      .then(res => {
+        axiosWithAuth().get('http://localhost:5000/api/colors')
+          .then(res => {
+            updateColors(res.data)
+          })
+          .catch(err => console.log(err))
+        console.log(res.data.payload);
+
+        // props.history.push('/friendslist')
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  const handleChange = (e) => {
+    setNewColor({ ...newColor, [e.target.name]: e.target.value })
+  }
+
+  const handleHexChange = (e) => {
+    setNewColor({...newColor, code: {hex: e.target.value}})
+  }
 
   return (
     <div className="colors-wrap">
@@ -93,11 +125,11 @@ const ColorList = ({ colors, updateColors }) => {
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+                e.stopPropagation();
+                deleteColor(color)
+              }
+              }>
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -140,6 +172,24 @@ const ColorList = ({ colors, updateColors }) => {
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
+
+      <form onSubmit={(e) => addColor(e)}>
+        <p>Color:</p>
+        <input
+          type='text'
+          name='color'
+          onChange={(e) => handleChange(e)}
+        />
+        <p>Hex:</p>
+        <input
+          type='text'
+          name='hex'
+          onChange={(e) => handleHexChange(e)}
+        />
+
+        <p></p>
+        <button>Add Color</button>
+      </form>
     </div>
   );
 };
