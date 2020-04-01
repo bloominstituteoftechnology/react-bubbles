8023337f9dbd3388d379cf10d581170b3ba3d534
Michael Lowe <mrlowememphis@yahoo.com>
2020-04-01 12:09:19 -0500

    MVP MET Styled components used

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Pack } from "@potion/layout";
-import { Svg, Circle } from "@potion/element";
+import React, { useState, useEffect } from 'react';
+import { Pack } from '@potion/layout';
+import { Svg, Circle } from '@potion/element';
 
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
@@ -13,7 +13,7 @@ const Bubbles = ({ colors }) => {
   }, [colors]);
 
   return (
-    <div className="bubble-wrap">
+   <div className='bubble-wrap'>
       <p>bubbles</p>
       <Svg width={400} height={400}>
         <Pack
@@ -24,8 +24,7 @@ const Bubbles = ({ colors }) => {
           size={[400, 400]}
           includeRoot={false}
           nodeEnter={d => ({ ...d, r: 0 })}
-          animate
-        >
+          animate>
           {nodes =>
             nodes
               .map(({ x, y, r, key }, i) => {
@@ -49,5 +48,62 @@ const Bubbles = ({ colors }) => {
     </div>
   );
 };
-
 export default Bubbles;
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { Pack } from "@potion/layout";
+// import { Svg, Circle } from "@potion/element";
+
+// const Bubbles = ({ colors }) => {
+//   const [bubbleData, setBubbleData] = useState([]);
+//   useEffect(() => {
+//     const generateBubbleData = colors.map((_, i) => ({
+//       value: Math.floor(Math.random() * (colors.length * 2)) + 1,
+//       key: `${i + 1}`
+//     }));
+//     setBubbleData(generateBubbleData);
+//   }, [colors]);
+
+//   return (
+//     <div className="bubble-wrap">
+//       <p>bubbles</p>
+//       <Svg width={400} height={400}>
+//         <Pack
+//           data={{
+//             children: bubbleData
+//           }}
+//           sum={datum => datum.value}
+//           size={[400, 400]}
+//           includeRoot={false}
+//           nodeEnter={d => ({ ...d, r: 0 })}
+//           animate
+//         >
+//           {nodes =>
+//             nodes
+//               .map(({ x, y, r, key }, i) => {
+//                 if (i < colors.length) {
+//                   return (
+//                     <Circle
+//                       key={key}
+//                       cx={x}
+//                       cy={y}
+//                       r={r}
+//                       fill={colors[i].code.hex}
+//                     />
+//                   );
+//                 }
+//                 return null;
+//               })
+//               .filter(v => v)
+//           }
+//         </Pack>
+//       </Svg>
+//     </div>
+//   );
+// };
+
+// export default Bubbles;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,111 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const Login = () => {
+import axios from 'axios';
+
+import styled from 'styled-components';
+
+const Login = props => {
+  const [user, setUser] = useState({ username: '', password: '' });
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const login = e => {
+    e.preventDefault();
+    axios
+    //post request to retrieve token
+      .post('http://localhost:5000/api/login', user)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/bubbles');
+      })
+      .catch(error => console.log(error));
+  };
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <Wrapper>
+      <h1>WELCOME 2 BUBBLES !!</h1>
+      
+      <Form onSubmit={login}>
+        <Input
+          type='text'
+          name='username'
+          value={user.username}
+          placeholder='Enter Username'
+          onChange={handleChange}
+        />
+        <Input
+          type='password'
+          name='password'
+          value={user.password}
+          placeholder='Enter Password'
+          onChange={handleChange}
+        />
+        <Button>Login</Button>
+      </Form>
+    </Wrapper>
   );
 };
 
 export default Login;
+
+// Adding styling using styled-components
+
+// Adding styling
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background:  	#2dc8aa;
+`;
+
+const Button = styled.button`
+  background: #ee9701;
+  border-radius: 20px;
+  padding: 10px;
+  margin: 5px;
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin: 10px;
+  border-radius: 20px;
+  border: 1px black;
+`;
+
+const Form = styled.form`
+  background: #89DCF5;
+  border-radius: 15px;
+  border: 7px ridge rgba(28,110,164,0.77);
+  padding: 40px;
+  margin: 0;
+  box-shadow: 10px 10px 8px #888888;
+`;
+
+
+
+
+
+// import React from "react";
+
+// const Login = () => {
+//   // make a post request to retrieve a token from the api
+//   // when you have handled the token, navigate to the BubblePage route
+//   return (
+//     <>
+//       <h1>Welcome to the Bubble App!</h1>
+//       <p>Build a login page here</p>
+//     </>
+//   );
+// };
+
+// export default Login;
