c7d4aa9666f807eb0809902166127377edb24428
thomas <t_huynh927@yahoo.com>
2020-05-01 19:12:54 -0700

    did first stretch

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -17,23 +17,35 @@ const ColorList = ({ colors, updateColors, getColorList }) => {
     setColorToEdit(color);
   };
 
+  const createColor = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post(`/api/colors`, colorToEdit)
+    .then(res => {
+      setColorToEdit(initialColor)
+      getColorList();
+    })
+    .catch(err => console.log(err));
+  }
+
   const saveEdit = e => {
     e.preventDefault();
     axiosWithAuth()
-    .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
-    .then(res => {
-      getColorList()
-    })
-    .catch(err => console.log(err))
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        setColorToEdit(initialColor)
+        getColorList();
+      })
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
     axiosWithAuth()
-    .delete(`/api/colors/${color.id}`)
-    .then(res => {
-      getColorList()
-    })
-    .catch(err => console.log(err))
+      .delete(`/api/colors/${color.id}`)
+      .then(res => {
+        getColorList();
+      })
+      .catch(err => console.log(err));
   };
 
   return (
@@ -43,12 +55,14 @@ const ColorList = ({ colors, updateColors, getColorList }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -85,12 +99,44 @@ const ColorList = ({ colors, updateColors, getColorList }) => {
           </label>
           <div className="button-row">
             <button type="submit">save</button>
+            <button onClick={() => {
+              setEditing(false)
+              setColorToEdit(initialColor)
+            }}>cancel</button>
+          </div>
+        </form>
+      )}
+      {!editing && (
+        <form onSubmit={createColor}>
+          <legend>create color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">create</button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
       )}
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
     </div>
   );
 };
