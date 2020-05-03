b3e667b3c46ce6576456debc5be1fad26a6f3b40
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-05-02 22:52:00 -0700

    MVP+ExtraWork

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BubblePage from './components/BubblePage';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import PrivateRoute from './components/PrivateRoute';
+
+import Login from './components/Login';
+import './styles.scss';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
+      <div className='App'>
+        <Switch>
+          <PrivateRoute exact path='/BubblePage' component={BubblePage} />
+          <Route path='/Login' component={Login} />
+          <Route component={Login} />
+        </Switch>
+        {/* <Route component={Login} /> */}
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
