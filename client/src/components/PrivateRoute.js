d5128caedcf9512bcfd5663f8f61b5963fca16f7
lex-marie790 <lexi.marie790@gmail.com>
2020-06-12 18:50:20 -0700

    PrivateRoute.js

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export const PrivateRoute = ({ component: Component, ...routeProps }) => {
+   return (
+       <Route 
+        {...routeProps}
+        render= {(props) => {
+            if (localStorage.getItem('token')) {
+                return <Component {...props} />
+            } else {
+                return <Redirect to='/login' />
+            }
+        }}
+       />
+   );
+};
