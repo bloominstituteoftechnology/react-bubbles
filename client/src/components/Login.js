6d37b00eca75bd369aadbd973522f000d8d82a04
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-06-15 13:16:28 -0700

    npm install, set uputils of axios, and structure of login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  //setting up the hooks
+  const [credentials, setCredentials]= useState({
+    username:'',
+    password:''
+  });
+  const {push} = useHistory();
+
+  //setting up on the onChange
+  const handleChange = e =>{
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+      axios.post()
+  }
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+          value={credentials.username}
+        />
+
+        <input 
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+          value={credentials.password}
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 };
