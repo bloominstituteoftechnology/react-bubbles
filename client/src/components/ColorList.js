64db814cfa3ad58c799e11647683eb00aa8a556c
Anthony <chitowncoder@gmail.com>
2020-06-14 21:28:42 -0500

    getting a 401 as I was cleaning up login

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,89 +1,175 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import uuid from 'uuid/v4';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+	color: '',
+	code: { hex: '' },
+	id: null,
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+	console.log(colors);
+	const [editing, setEditing] = useState(false);
+	const [adding, setAdding] = useState(false);
+	const [colorToEdit, setColorToEdit] = useState(initialColor);
+	const [colorToAdd, setColorToAdd] = useState(initialColor);
 
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
+	const editColor = (color) => {
+		setEditing(true);
+		setAdding(false);
+		setColorToEdit(color);
+		setColorToAdd(initialColor);
+	};
 
-  const saveEdit = e => {
-    e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-  };
+	const addColor = () => {
+		setEditing(false);
+		setAdding(true);
+		setColorToEdit(initialColor);
+		setColorToAdd(initialColor);
+	};
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
-  };
+	const saveColor = (e) => {
+		e.preventDefault();
+		axiosWithAuth()
+			.post(`/colors`, colorToAdd)
+			.then((res) => {
+				console.log('POST', res);
+				setAdding(false);
+				updateColors([...colors, { ...colorToAdd, id: uuid() }]);
+				setColorToAdd(initialColor);
+			})
+			.catch((err) => console.error(err));
+	};
 
-  return (
-    <div className="colors-wrap">
-      <p>colors</p>
-      <ul>
-        {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{ backgroundColor: color.code.hex }}
-            />
-          </li>
-        ))}
-      </ul>
-      {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
-            <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
-            <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
-      )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
-    </div>
-  );
+	const saveEdit = (e) => {
+		e.preventDefault();
+		// Make a put request to save your updated color
+		// think about where will you get the id from...
+		// where is is saved right now?
+		axiosWithAuth()
+			.put(`/colors/${colorToEdit.id}`, colorToEdit)
+			.then((res) => {
+				setEditing(false);
+				updateColors(
+					colors.map((item) => {
+						if (item.id === colorToEdit.id) {
+							return { ...colorToEdit };
+						}else{
+							return { ...item };
+						}
+						
+					})
+				);
+				setColorToEdit(initialColor);
+			})
+			.catch((err) => console.error(err));
+	};
+
+	const deleteColor = (color) => {
+		axiosWithAuth()
+			.delete(`/colors/${color.id}`)
+			.then((res) => {
+				setEditing(false);
+				updateColors(colors.filter((item) => item.id !== color.id));
+				setColorToEdit(initialColor);
+			})
+			.catch((err) => console.error(err));
+	};
+
+	return (
+		<div className="colors-wrap">
+			<p>colors</p>
+			<ul>
+				{colors.map((color, index) => (
+					<li key={index} onClick={() => editColor(color)}>
+						<span>
+							<span
+								className="delete"
+								onClick={(e) => {
+									e.stopPropagation();
+									deleteColor(color);
+								}}
+							>
+								x
+							</span>{' '}
+							{color.color}
+						</span>
+						<div
+							className="color-box"
+							style={{ backgroundColor: color.code.hex }}
+						/>
+					</li>
+				))}
+			</ul>
+
+			<button onClick={() => addColor()}>Add Color</button>
+			{editing && (
+				<form onSubmit={saveEdit}>
+					<legend>edit color</legend>
+					<label>
+						color name:
+						<input
+							onChange={(e) =>
+								setColorToEdit({ ...colorToEdit, color: e.target.value })
+							}
+							value={colorToEdit.color}
+						/>
+					</label>
+					<label>
+						hex code:
+						<input
+							type="color"
+							onChange={(e) =>
+								setColorToEdit({
+									...colorToEdit,
+									code: { hex: e.target.value },
+								})
+							}
+							value={colorToEdit.code.hex}
+						/>
+					</label>
+					<div className="button-row">
+						<button type="submit">save</button>
+						<button onClick={() => setEditing(false)}>cancel</button>
+					</div>
+				</form>
+			)}
+			{adding && (
+				<form onSubmit={saveColor}>
+					<legend>add color</legend>
+					<label>
+						color name:
+						<input
+							onChange={(e) =>
+								setColorToAdd({ ...colorToAdd, color: e.target.value })
+							}
+							value={colorToAdd.color}
+						/>
+					</label>
+					<label>
+						hex code:
+						<input
+							type="color"
+							onChange={(e) =>
+								setColorToAdd({
+									...colorToAdd,
+									code: { hex: e.target.value },
+								})
+							}
+							value={colorToAdd.code.hex}
+						/>
+					</label>
+					<div className="button-row">
+						<button type="submit">save</button>
+						<button onClick={() => setAdding(false)}>cancel</button>
+					</div>
+				</form>
+			)}
+			<div className="spacer" />
+			{/* stretch - build another form here to add a color */}
+		</div>
+	);
 };
 
 export default ColorList;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+const Login = (props) => {
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
+
+	const [login, setLogin] = useState({
+		username: '',
+		password: '',
+	});
+
+	function handleChange(e) {
+		setLogin({
+			...login,
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		axiosWithAuth()
+			.post('/login', login)
+			.then((res) => {
+				localStorage.setItem('token', res.data.payload);
+				props.history.push('/bubble-page');
+			})
+			.catch((err) => console.error(err));
+	}
+
+	return (
+		<>
+			<h1>Welcome to the Bubble App!</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="username"
+					value={login.username}
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					name="password"
+					value={login.password}
+					onChange={handleChange}
+				/>
+				<button>Login</button>
+			</form>
+		</>
+	);
 };
 
 export default Login;
