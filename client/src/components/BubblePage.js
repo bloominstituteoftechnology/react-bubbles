03438916c216ae70b032173cf30555261a6901df
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-06-15 14:07:56 -0700

    trying to get to render the colorlist

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import axiosWithAuth from "../utils/axiosWithAuth";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import {useHistory} from 'react-router-dom';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const history = useHistory();
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(()=>{
+    axiosWithAuth().get("/api/colors")
+      .then(res => {
+        console.log('bubblepage.js: ', res)
+        setColorList(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  const logOut = () =>{
+    localStorage.clear()
+    history.push('/')
+
+  }
   return (
     <>
+      <button onClick={logOut}>Log out</button>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
