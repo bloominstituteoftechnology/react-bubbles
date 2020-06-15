66be31c30d6726e27e36870b8b53527ec9daf4d1
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-06-14 21:08:39 -0700

    issues with save bubbles

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,50 +1,67 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useParams } from 'react-router-dom';
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+  color: '',
+  code: { hex: '' }
 };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+const ColorList = ({ colors, getColors }) => {
+  
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const editColor = color => {
+   const { id } = useParams();
+
+  const editColor = (color) => {
     setEditing(true);
     setColorToEdit(color);
   };
 
+  
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-  };
 
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/:id/${id}`, colorToEdit)
+      .then(res => {
+        getColors();
+      })
+      .catch(err => console.error('error', err.message));
+  };
+  
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+          getColors()
+      })
+      .catch(err => {
+          console.error(err.message, err.response)
+      });
   };
 
   return (
-    <div className="colors-wrap">
-      <p>colors</p>
+    <div className='colors-wrap'>
+      <p>Colors</p>
       <ul>
-        {colors.map(color => (
+        {colors.map((color) => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
+              <span
+                className='delete'
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
+              </span>{' '}
               {color.color}
             </span>
             <div
-              className="color-box"
+              className='color-box'
               style={{ backgroundColor: color.code.hex }}
             />
           </li>
@@ -56,14 +73,14 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             color name:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
               }
               value={colorToEdit.color}
             />
           </label>
           <label>
-            hex code:
+            Hex code:
             <input
               onChange={e =>
                 setColorToEdit({
@@ -74,14 +91,14 @@ const ColorList = ({ colors, updateColors }) => {
               value={colorToEdit.code.hex}
             />
           </label>
-          <div className="button-row">
-            <button type="submit">save</button>
+          <div className='button-row'>
+            <button type='submit'>save</button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
       )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      <div className='spacer' />
+     
     </div>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,78 @@
-import React from "react";
+import React, { useState } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+
+  
+
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // const login = e => {
+  //   e.preventDefault();
+  //       axios
+  //     .post('http://localhost:5000/api/colors', credentials)
+  //     .then(res => {
+  //       localStorage.setItem('token', res.data.payload);
+  //       push('/BubblePage');
+  //     })
+  //     .catch(err => {
+  //         console.error(err.message, err.response)
+  //     });
+  // };
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+        .post('http://localhost:5000/api/login', credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload)
+            props.history.push('/BubblePage')
+        })
+        .catch(err => {
+            console.error('There was an error', err)
+        })
+}
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+          <input
+            type='text'
+            placeholder='Username'
+            name='username'
+            onChange={handleChange}
+            value={credentials.username}
+          />
+  
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            onChange={handleChange}
+            value={credentials.password}
+          />
+      
+
+        <div className='loginButton'>
+          <button disabled={!credentials}>Log In</button>
+        </div>
+      </form>
     </>
   );
 };
