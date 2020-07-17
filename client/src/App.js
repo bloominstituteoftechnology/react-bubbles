d90ba9cd9c44aef460c434cc02db8231e3874fd7
Ben Venker <bvemails@gmai.com>
2020-07-17 18:35:01 -0500

    get back colors list

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
+
+import './styles.scss';
 
 function App() {
   return (
@@ -13,6 +16,11 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute
+          exact
+          path="/protected"
+          component={BubblePage}
+        ></PrivateRoute>
       </div>
     </Router>
   );
